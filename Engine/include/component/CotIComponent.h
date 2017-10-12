9a009aa2a59520cee9802caf4080666696ef2e27
맛카치 <mkachi@naver.com>
2017-10-12 11:59:16 +0900

    Add Component interface

---
+++ Engine/include/component/CotIComponent.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "base/CotTime.h"
+
+namespace Cot
+{
+	class COT_API IComponent
+	{
+		COT_INTERFACE(IComponent);
+	private:
+		string _type;
+
+	public:
+		virtual void OnEnable()			{}
+		virtual void Reset()			{}
+		virtual void Awake()			{}
+		virtual void Start()			{}
+		virtual void FixedUpdate()		{}
+		virtual void Update(Time& time)	{}
+		virtual void LateUpdate()		{}
+		virtual void OnDisable()		{}
+		virtual void OnDestroy()		{}
+	};
+}
